df59ab8572b5709103539333f9c711d5b13fd1d7
Professor1345 <alfulaani1345@gmail.com>
2024-03-19 18:30:43 +0100

    Components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,18 @@ import About from "./components/About";
 import GetApp from "./components/Get App";
 import Footer from "./components/Footer";
 
-const page = () => {
-  return <div className="font-bold">page</div>;
-};
+interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-  return <div>Hello World!</div>
-}
+  return (
+    <div>
+      <Home />
+      <Services />
+      <About />
+      <GetApp />
+      <Footer />
+    </div>
+  );
+};
 
-export default Page
+export default Page;
